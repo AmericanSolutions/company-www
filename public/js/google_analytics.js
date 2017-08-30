d094135ec48340fe09ac0fce7805ae96a7384df0
wingchunkf <bergmcj@comcast.net>
2017-08-30 11:01:46 -0700

    fixed incorrect ga tracking id number

--- public/js/google_analytics.js
+++ public/js/google_analytics.js
@@ -1,23 +1,22 @@
 // google analytics code for production
 
-// if (document.location.hostname === "americandevshop.com") { 
+if (document.location.hostname === "americandevshop.com") { 
 
-/*(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-105525048-1', 'auto');
+ga('create', 'UA-105170975-1', 'auto');
 ga('send', 'pageview');
 
-} */
-
-
-ga('create', 'UA-105525048-1', 'auto');
+} 
+/*
+ga('create', 'UA-105170975-1', 'auto');
 ga('set', 'checkProtocolTask', null); // Disable file protocol checking. 
 ga('set', 'checkStorageTask', null); // Disable cookie storage checking. 
 ga('set', 'historyImportTask', null); // Disable history checking (requires reading from cookies). 
-ga('send', 'pageview'); 
+ga('send', 'pageview'); */
 
 const phoneClick = document.querySelector('.phone');
   
@@ -29,7 +28,7 @@ const phoneClick = document.querySelector('.phone');
 const scheduleCall = document.querySelector('.schedule-call');
   
   scheduleCall.addEventListener('click', () => {    
-      ga('send', 'event', el.innerHTML, 'clicked', 'schedule call');
+      ga('send', 'event', 'call scheduled', 'clicked', 'schedule call');
       console.log('schedule call');
   })
 
@@ -38,6 +37,13 @@ const email = document.querySelector('.email');
   email.addEventListener('click', () => {		
    	  ga('send', 'event', 'email address', 'clicked', 'email');
    	  console.log('email clicked');
+  })
+
+  const home = document.querySelector('.nav-home');
+  
+  home.addEventListener('click', () => {   
+      ga('send', 'event', 'home', 'clicked', 'home nav');
+      console.log('home clicked');
   })
 
 document.querySelectorAll('.nav-item').forEach(el => {
